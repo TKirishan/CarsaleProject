195be3dad3631b868de86e412807c7a8d926f3f2
TKirishan <kiri2000vasu@gmail.com>
2024-06-08 21:09:47 +0530

    secound time commit

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -5,7 +5,8 @@ import { Route,Routes } from 'react-router-dom';
 import "./Project.css";
 import { FaApple, FaFacebook } from 'react-icons/fa';
 import { FaGoogle } from "react-icons/fa6";
-import MyBackgroundImage from './range-rover.png';
+
+//import BackgroundImage from './image/range-rover.png';
 
 
 function Signin(){
@@ -21,49 +22,50 @@ function Signin(){
     
 
     return(
-      <div className="maindoy" > 
-        {/* <div className="body"> */}
-            <div className="header"><br/>
-                <h1><b>CarSales.com</b></h1>
+      <div className="bigbody">
 
-            </div>
-
-            <div className="content">
-                <div className="leftcontent">
-                    <h1>SignIn</h1>
-
-                    <h3>Use your email or mobile number</h3><br/>
-                    <form onSubmit={handleSubmit}>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} 
-                    id="email" placeholder="Enter your email or mobile number"/><br /><br/><br/><br/><br/><br/><br/>
-                    
-                    <button type="submit" id="btn1"><b>Create Accountn</b></button>
-
-                    <button type="submit" id="btn2"><b>Next Step</b></button>
-                    </form>
+                <div className="header"><br/>
+                    <h1><b>CarSales.com</b></h1>
 
                 </div>
 
-                <div className="rightcontent">
-                <h3>Continue with Single Sign-On</h3><br />
-                
-                <button className="apple-button" id="btn3"><FaApple /> <b>Continue with Apple</b></button><br/><br/><br/>
-                
-                <button className="google-button" id="btn3"><FaGoogle /> <b>Continue with Google</b></button><br/><br/><br/>
-              
-                <button className="facebook-button" id="btn3"><FaFacebook /> <b>Continue with Facebook</b></button><br/><br/><br/>
-                
-                
+                <div className="content">
 
-                <image src="../images/apple.png" id="apple"/>
+                    <div className="leftcontent">
+                        <h1>SignIn</h1>
+                        <h3>Use your email or mobile number</h3><br/>
+                        <form onSubmit={handleSubmit}>
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} 
+                        id="email" placeholder="Enter your email or mobile number"/><br /><br/><br/><br/><br/><br/><br/>
+                        
+                        <button type="submit" id="btn1"><b>Create Account</b></button>
+
+                        <button type="submit" id="btn2"><b>Continue</b></button>
+                        </form>
+
+                    </div>
+
+                    <div className="rightcontent">
+                    <h3>Continue with Single Sign-On</h3><br />
+                    <button className="apple-button" id="btn3"><FaApple /> <b>Continue with Apple</b></button><br/><br/><br/>
+                    
+                    <button className="google-button" id="btn3"><FaGoogle /> <b>Continue with Google</b></button><br/><br/><br/>
+                  
+                    <button className="facebook-button" id="btn3"><FaFacebook /> <b>Continue with Facebook</b></button><br/><br/><br/>
+                    
+                    
+                    {/* <img src={MyBackgroundImage} alt=""/> */}
+                    {/* <image src="../images/apple.png" id="apple"/> */}
+
+                    
+
+
+                    </div>
+
+                </div>
                 
-
-
-                 </div>
-
-            </div>
-            
-        {/* </div> */}
+           
+          
       </div>
     );
   }
